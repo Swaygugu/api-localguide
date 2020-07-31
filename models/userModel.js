a6a9cf6bcf492df3791fc2e681f999f508f020b3
Nong <worakamon.pi@baac.or.th>
2020-07-31 16:09:28 +0700

    fff

--- models/userModel.js
+++ models/userModel.js
@@ -2,11 +2,33 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const Schema = mongoose.Schema;
 
-const userSchema = Schema({
-  name:  { type: String, required: true, trim: true },
+const guideSchema =  Schema({
+  firstname:  { type: String, required: true, trim: true },
+  lastname:  { type: String, required: true, trim: true },
+  userid:  { type: String, required: true, trim: true },
   email: { type: String, required: true, trim: true, unique: true, index: true },
   password: { type: String, required: true, trim: true , minlength: 3 },
-  role: { type: String, default: 'member' }
+  usertype: { type: String, required: true, trim: true  },
+  gender: { type: String, required: true, trim: true  },
+  address: { type: Array},
+  education: { type: String, required: true, trim: true  },
+  displayname: { type: String, required: true, trim: true  },
+  profilepicture: { type: String, required: true, trim: true  },
+  certificate: { type: String, required: true, trim: true  },
+  location: { type: Array  },
+  
+},{
+  collection: 'users'
+});
+
+const userSchema =  Schema({
+  firstname:  { type: String, required: true, trim: true },
+  lastname:  { type: String, required: true, trim: true },
+  userid:  { type: String, required: true, trim: true },
+  email: { type: String, required: true, trim: true, unique: true, index: true },
+  password: { type: String, required: true, trim: true , minlength: 3 },
+  usertype: { type: String, required: true, trim: true  },
+ 
 },{
   collection: 'users'
 });
@@ -23,7 +45,20 @@ userSchema.methods.comparePassword = async function (password)  {
   const isValid = await bcrypt.compare(password, this.password);
   return isValid;
 }
+
+guideSchema.methods.encryptPassword = async(password) =>{
+  const salt = await bcrypt.genSalt(10);
+  const hashPassword = await bcrypt.hash(password,salt);
+  return hashPassword;
+}  
+
+guideSchema.methods.comparePassword = async function (password)  {
+  console.log(password);
+  console.log(this.password);
+  const isValid = await bcrypt.compare(password, this.password);
+  return isValid;
+}
+  const user = mongoose.model('User',userSchema);
   
-  const user = mongoose.model('User', userSchema);
+  module.exports = user;
   
-  module.exports = user;;
